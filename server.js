5693756be5755b80946d2be03ccda467f5562376
YJ <yair.yoseph@gmail.com>
2018-10-18 14:36:46 -0700

    port fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 //initialize express
 var app = express();
